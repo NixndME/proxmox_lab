fda0d29e6c92015bdbe931d58115024f27749325
NeilJvR <neil.vanrensburg@gmail.com>
2024-04-10 16:37:13 +0200

    Update VirtualImageSync.groovy

--- src/main/groovy/com/morpheus/proxmox/ve/sync/VirtualImageSync.groovy
+++ src/main/groovy/com/morpheus/proxmox/ve/sync/VirtualImageSync.groovy
@@ -8,11 +8,14 @@ import com.morpheusdata.core.data.DataQuery
 import com.morpheusdata.core.providers.CloudProvider
 import com.morpheusdata.core.util.HttpApiClient
 import com.morpheusdata.core.util.SyncTask
+import com.morpheusdata.model.Account
 import com.morpheusdata.model.Cloud
 import com.morpheusdata.model.CloudPool
+import com.morpheusdata.model.ImageType
 import com.morpheusdata.model.Network
 import com.morpheusdata.model.OsType
 import com.morpheusdata.model.VirtualImage
+import com.morpheusdata.model.VirtualImageLocation
 import com.morpheusdata.model.projection.NetworkIdentityProjection
 import com.morpheusdata.model.projection.VirtualImageIdentityProjection
 import groovy.util.logging.Slf4j
@@ -50,8 +53,8 @@ class VirtualImageSync {
             log.debug("Proxmox templates found: $cloudItems")
 
             Observable domainRecords = context.async.virtualImage.listIdentityProjections(new DataQuery().withFilter(
-                    new DataFilter("refType", "ComputeZone")).withFilter(
-                    new DataFilter("refId", cloud.id)))
+                    new DataFilter("account", cloud.account))
+            )
 
             SyncTask<VirtualImageIdentityProjection, Map, VirtualImage> syncTask = new SyncTask<>(domainRecords, cloudItems)
             syncTask.addMatchFunction { VirtualImageIdentityProjection domainObject, Map cloudItem ->
@@ -81,29 +84,10 @@ class VirtualImageSync {
         def adds = []
         addList.each {
             log.debug("Creating virtual image: $it")
-            adds << new VirtualImage(
-                installAgent: false,
-                externalId: it.vmid,
-                imageType: qcow2,
-                name: it.name,
-                ownerId: cloud.account.id,
-                systemImage: false,
-                deleted: false,
-                visibility: "visible",
-                owner: cloud.account,
-                account: cloud.account,
-                code: "proxmox.image.${it.vmid}",
-                architecture: "qemu",
-                isPublic: true,
-                minDisk: it.maxdisk,
-                minRam: it.maxmem,
-                status: 'Active',
-                osType: new OsType(name: "Proxmox Image", platform: "linux"),
-                category: 'proxmox.image',
-                platform: "linux",
-                refType: 'ComputeZone',
-                refId: cloud.id,
-            )
+            VirtualImage virtImg = new VirtualImage(buildVirtualImageConfig(it))
+            VirtualImageLocation virtImgLoc = buildLocationConfig(virtImg)
+            virtImg.imageLocations = [virtImgLoc]
+            adds << virtImg
         }
 
         log.debug "About to create ${adds.size()} virtualImages"
@@ -112,17 +96,72 @@ class VirtualImageSync {
 
 
     private updateMatchedVirtualImages(List<SyncTask.UpdateItem<VirtualImage, Map>> updateItems) {
+        def saves = []
         for (def updateItem in updateItems) {
             def existingItem = updateItem.existingItem
             def cloudItem = updateItem.masterItem
+            def saveIt = false
 
-            //Add update logic here...
-            //updateMachineMetrics()
+            log.info("Updating VirtualImage $existingItem.name")
+
+            def existingLocation = existingItem.imageLocations.find{
+                it.refType == 'ComputeZone' && it.refId == cloud.id
+            }
+            if (!existingLocation) {
+                existingItem.imageLocations << new VirtualImageLocation(buildLocationConfig(existingItem))
+                saveIt = true
+            }
+            //Add other update logic here...
+            if (saveIt) {
+                saves << existingItem
+            }
+        }
+        if (saves) {
+            context.async.virtualImage.save(saves, cloud).blockingGet()
         }
 
-        //Example:
-        // Nutanix - https://github.com/gomorpheus/morpheus-nutanix-prism-plugin/blob/master/src/main/groovy/com/morpheusdata/nutanix/prism/plugin/sync/ImagesSync.groovy
-        // Openstack - https://github.com/gomorpheus/morpheus-openstack-plugin/blob/main/src/main/groovy/com/morpheusdata/openstack/plugin/sync/ImagesSync.groovy
+
+    }
+
+
+    private Map buildLocationConfig(VirtualImage image) {
+        return [
+                virtualImage: image,
+                code        : "nutanix.prism.image.${cloud.id}.${image.externalId}",
+                internalId  : image.internalId,
+                externalId  : image.externalId,
+                imageName   : image.name,
+                imageRegion : cloud.regionCode,
+                isPublic    : false,
+                refType     : 'ComputeZone',
+                refId       : cloud.id
+        ]
+    }
+
+
+    private buildVirtualImageConfig(Map cloudItem) {
+        Account account = cloud.account
+        def regionCode = cloud.regionCode
+
+        def imageConfig = [
+                account    : account,
+                category   : "nutanix.prism.image.${cloud.id}",
+                name       : cloudItem.status.name,
+                code       : "nutanix.prism.image.${cloud.id}.${cloudItem.metadata.uuid}",
+                imageType  : ImageType.qcow2,
+                status     : 'Active',
+                minDisk    : cloudItem.status.resources.size_bytes?.toLong(),
+                isPublic   : false,
+                remotePath : cloudItem.status.resources?.retrieval_uri_list?.getAt(0) ?: cloudItem.status.resources?.source_uri,
+                externalId : cloudItem.metadata.uuid,
+                imageRegion: regionCode,
+                internalId : cloudItem.metadata.uuid,
+                uniqueId   : cloudItem.metadata.uuid,
+                bucketId   : cloudItem.status.resources?.current_cluster_reference_list?.getAt(0)?.uuid,
+                systemImage: false
+        ]
+
+        return imageConfig
     }
 
 
